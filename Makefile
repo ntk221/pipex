2072d08deec868fd6107bcbc265265de72daf88c
ntk221 <nittakdub@gmail.com>
2023-01-25 02:28:46 +0900

    fix

--- Makefile
+++ Makefile
@@ -2,12 +2,13 @@ CC = gcc
 CFLAGS = -Wall -Werror -Wextra
 
 SRCS = main.c
+LIBFT = libft
 NAME = pipex
 
 all: $(NAME)
 
 $(NAME):
-	$(CC) $(SRCS) -o $(NAME)
+	$(CC) $(SRCS) -L$(LIBFT) -o $(NAME) -lft
 
 rm:
 	rm $(NAME)
--- main.c
+++ main.c
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kazuki <kazuki@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 01:21:42 by kazuki            #+#    #+#             */
+/*   Updated: 2023/01/25 02:28:30 by kazuki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include "include/get_next_line.h"
+// #include "include/get_next_line.h"
+#include "include/libft.h"
 
 // int open(const char *path, int oflag, ...);
 // ssize_t read(int fildes, void *buf, size_t nbyte);
@@ -11,45 +24,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int get_arg2(char *c, char *arg[], char *sym)
+void  error(void)
 {
-    int i = 0;
-    arg[i] = strtok(c, sym);
-    while(arg[i] != NULL)
-    {
-        i++;
-        arg[i] = strtok(NULL, sym);
-    }
-    return i;
+  puts("error");
+  exit(1);
 }
 
 int main(int argc, char **argv)
 {
   int   fd[2];
   pid_t pid;
-  char  *arg_c1[32], *arg_c2[32];
+  char  **arg_c1, **arg_c2;
   int   st;
 
-
   // TODO: error ハンドリング
+
+  int infile = open(argv[1], O_RDONLY);
+  if (infile < 0)
+    error();
+
+  int outfile = open(argv[4], O_WRONLY);
+  if (outfile < 0)
+    error();
+
   int ret1 = pipe(fd);
   if (ret1 < 0)
-  {
-      perror("pipe");
-      exit(1);
-  }
+    error();
 
-  get_arg2(argv[2], arg_c1, " ");
-  get_arg2(argv[3], arg_c2, " ");
+  arg_c1 = ft_split(argv[2], ' ');
+  arg_c2 = ft_split(argv[3], ' ');
   /*for(int i = 0; arg_c2[i] != NULL; i++)
     printf("%s\n", arg_c2[i]);*/
 
   pid = fork();
   if (pid == 0)
   {
-    int infile = open(argv[1], O_RDONLY);
     dup2(infile, 0);
-    close(infile);
+    // close(infile);
     dup2(fd[1], STDOUT_FILENO);
     execv(arg_c1[0], arg_c1);
     perror("error");
@@ -59,12 +70,15 @@ int main(int argc, char **argv)
   if (pid == 0)
   {
     int outfile = open(argv[4], O_WRONLY);
-    dup2(outfile, fd[1]);
+    dup2(4, fd[1]); // ?????
+    //close(fd[1]);
+    //close(STDOUT_FILENO);
     dup2(fd[0], STDIN_FILENO);
+    // close(fd[1]);
     execv(arg_c2[0], arg_c2);
     perror("error");
     exit(99);
-  }
+  } 
   close(fd[0]);
   close(fd[1]);
   wait(&st);

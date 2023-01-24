c7bf9dc0ff0e7fd62078d5724b0635affdc1481d
ntk221 <nittakdub@gmail.com>
2023-01-24 21:57:25 +0900

    finish #4

---
+++ learning/fork.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+    int     infile;
+    pid_t   pid;
+
+    pid = fork();
+    if (pid == 0) //子プロセス
+    {
+        infile = open("test.txt", O_RDONLY);
+        dup2(infile, 0);
+        execl("/bin/cat", "cat", NULL);
+        perror("/bin/cat");
+        exit(1);
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status))
+            printf("exit status=%d\n", WEXITSTATUS(status));
+        else if (WIFSIGNALED(status))
+            printf("signal sig=%d\n", WTERMSIG(status));
+        else
+            printf("abnormal exit\n");
+        exit(0);
+    }
+}
